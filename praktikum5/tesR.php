153dedede436303c5e72803f7c0eec86f5f7e576
baitun16 <61089675+baitun16@users.noreply.github.com>
2020-03-09 10:08:53 +0700

    Add files via upload

---
+++ praktikum5/tesR.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="refresh" content="2; url= http://localhost/praktikum5/mod_1.php" >
+	<title></title>
+</head>
+<body>
+Tes Redirect
+</body>
+</html>
